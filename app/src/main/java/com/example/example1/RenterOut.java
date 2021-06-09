bdec68e1877f8df574d2c0d629d105a489c1ba1c
monayaseen <moooonah15_7@icloud.com>
2021-06-09 17:44:54 +0300

    removed the spaces

---
+++ app/src/main/java/com/example/example1/RenterOut.java
@@ -0,0 +1,28 @@
+package com.example.example1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RenterOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter_out);
+
+        Spinner dropdown = findViewById(R.id.item_category);
+        String[] items = new String[]{"Home", "Clothes", "Electrical devices", "Accessories","Events"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        Spinner dropdown2 = findViewById(R.id.item_job);
+        String[] items2 = new String[]{"Student", "Housewife", "Teacher", "Farmer", "Other"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+
+    }
+}
